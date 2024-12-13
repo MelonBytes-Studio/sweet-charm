6c5b9ff379aa779e95513c3b52929ae6d7756fae
SUMER <9999smexov@gmail.com>
2024-12-13 06:58:47 +0300

    fix: now you can get raw charm atom from readonly atom

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,6 @@
 import type { AtomClass } from "atom-class";
 import type { DeepPartialWithNone } from "sync-utils";
+import type { Atom } from "@rbxts/charm";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type AtomTable = Record<string, AtomClass<any>>;
@@ -29,6 +30,7 @@ export type Payload<T extends AtomTable = AtomTable> = InitPayload<T> | PatchPay
 
 export interface ReadonlyAtom<T> {
 	getData(): T;
+	getAtom(): Atom<T>;
 	subscribe(callback: (newState: T, previous: T) => void): () => void;
 	subscribe<Y>(selector: (newState: T) => Y, callback: (newState: Y, previous: Y) => void): () => void;
 }
